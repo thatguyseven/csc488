1022da1aa88351e067932ade0ac9485f160eeda9
thatguyseven <aaronni0718@gmail.com>
2025-04-20 17:13:00 -0400

    Refactored app.py to run with current redis version

--- my-csc488-hws/lab7/app.py
+++ my-csc488-hws/lab7/app.py
@@ -33,8 +33,26 @@ def load_to_redis():
         # Stores landing sites into Redis database
         for site_id, row in enumerate(data["meteorite_landings"], start=1):
                 key = f"{site_id}"
-                rd.hset(key, mapping=row)
-        
+                # DEBUG: Print type and content of data row
+                # print(f"Row {site_id}: {row}")
+                # print(f"Type: {type(row)}, Content: {row}")
+                if not isinstance(row, dict):
+                        print(f"Skipping invalid row at index {site_id}: {row}")
+                        continue
+
+                clean_row = {str(k): str(v) for k, v in row.items()}
+
+                # DEV NOTE: The reason why this does not work is likely due to some incongruity with the redis server and the version of redis used.
+                try:
+                        # DEBUG: Logging print functions 
+                        # print(f"Row {site_id}: {clean_row}")
+                        # print(f"Type: {type(clean_row)}, Content: {clean_row}")
+                        # Iterate through the clean_row and set each field-value pair
+                        for field, value in clean_row.items():
+                                rd.hset(key, field, value)
+                except Exception as e:
+                        print(f"Redis insert failed for key {key}: {e}")
+                        
         # Return success message
         return jsonify({"message": f"Successfully loaded {len(data["meteorite_landings"])} meteorite landings into Redis."}), 200
 
